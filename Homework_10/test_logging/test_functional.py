8135ea030cfa7f8306a8842b0b8aeaf016e4be61
ViktoriiaTimon <zurk2007@gmail.com>
2024-10-30 12:02:16 +0100

    Have made changes in tests - the readlines() is added to select the last log in the file

--- Homework_10/test_logging/test_functional.py
+++ Homework_10/test_logging/test_functional.py
@@ -1,19 +1,24 @@
 import logging
-
 from .test_homework_10 import log_event
-
 import pytest
 
-class TestUserLogging():
 
+class TestUserLogging:
     @pytest.mark.parametrize("username, status, expected_level",
-    [
-        ("Tom", "success", logging.INFO),
-        ("Tom", "expired", logging.WARNING),
-        ("Tom", "failed", logging.ERROR),
-        ("Tom", None, logging.ERROR),
-        ("Tom", " ", logging.ERROR),
-    ])
-
+                             [
+                                 ("Tom", "success", 'INFO'),
+                                 ("Tom", "expired", 'WARNING'),
+                                 ("Tom", "failed", 'ERROR'),
+                             ])
     def test_logging_levels(self, username, status, expected_level):
-        log_event(username, status) == expected_level
+        log_event(username, status)
+
+        file_log = "login_system.log"
+        with open(file_log, mode='r') as f:
+            line = f.readlines()
+            last_log = line[-1].strip()
+
+        log_content = f"Login event - Username: {username}, Status: {status} - {expected_level}"
+
+        assert log_content in last_log, f"{log_content} not found in the last log: {last_log}"
+
