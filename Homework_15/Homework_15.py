9558dd038f7eeb153836c0ecc582fda5a0fdceba
ViktoriiaTimon <zurk2007@gmail.com>
2024-11-13 18:29:01 +0100

    Homework_15 completed

---
+++ Homework_15/Homework_15.py
@@ -0,0 +1,35 @@
+class Rombus:
+    def __init__(self, side_a, conner_a):
+        self.side_a = side_a
+        self.conner_a = conner_a
+        self.conner_b = 180 - conner_a
+
+    def __setattr__(self, name, value):
+        if name == "side_a":
+            if value <= 0:
+                print("Side must be greater than 0.")
+            else:
+                self.__dict__[name] = value
+        elif name == "conner_a":
+            if 0 <= value >= 180:
+                print(f"Conner '{name}' must be between 0 and 180 degrees.")
+            else:
+                self.__dict__[name] = value
+                self.__dict__["conner_b"] = 180 - value
+        elif name == "conner_b":
+            if 0 <= value >= 180:
+                print(f"Conner '{name}' must be between 0 and 180 degrees.")
+            else:
+                if self.__dict__.get("conner_a", 0) + value != 180:
+                    print("The sum of conner must be 180 degrees.")
+                else:
+                    self.__dict__[name] = value
+                    self.__dict__["conner_a"] = 180 - value
+
+# Testing the code
+romb = Rombus(10, 99)
+print(romb.conner_a)
+print(romb.conner_b)
+
+romb.conner_a = 70
+print(romb.conner_b)
