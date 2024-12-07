e5dab2ff58b17331a0f8726856522ae883ed02a8
ViktoriiaTimon <zurk2007@gmail.com>
2024-12-07 20:27:28 +0100

    Homework_19_2.py changes are made for with section

--- Homework_19/Homework_19_2.py
+++ Homework_19/Homework_19_2.py
@@ -13,8 +13,9 @@ if new_image.status_code == 200:
         image.write(new_image.content)
 
     with open('new_image.jpg', "rb") as image:
-        files = {"image": image}
-        post_response = requests.post(upload_url, files=files)
+        image_1 = image.read()
+    files = {"image": image_1}
+    post_response = requests.post(upload_url, files=files)
 
     if post_response.status_code == 201:
         image_url = post_response.json().get("image_url")
@@ -24,15 +25,15 @@ if new_image.status_code == 200:
 
 image_url = 'http://127.0.0.1:8080/uploads/new_image.jpg'
 
-get_uploaded_image = requests.get('http://127.0.0.1:8080/uploads/new_image.jpg')
+get_uploaded_image = requests.get('http://127.0.0.1:8080/upload/new_image.jpg')
 if get_uploaded_image.status_code == 200:
     with open('new_image.jpg', 'wb') as test_image:
         test_image.write(get_uploaded_image.content)
-        print('File is here')
-print(get_uploaded_image.json())
+    print('File is here')
+    print(get_uploaded_image.json())
 
 # DELETE image
-url = 'http://127.0.0.1:8080/uploads/new_image.jpg'
+url = 'http://127.0.0.1:8080/upload/new_image.jpg'
 response = requests.delete(url)
 
 if response.status_code == 200:
