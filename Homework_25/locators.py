b4a06fbc5d25beacd7bd5fbffb56f5493b5be188
ViktoriiaTimon <zurk2007@gmail.com>
2024-12-28 22:51:00 +0100

    locators task

---
+++ Homework_25/locators.py
@@ -0,0 +1,54 @@
+#CSS
+div#player
+div#player .html5-video-player.ytp-hide-controls.ytp-exp-bottom-control-flexbox.ytp-modern-caption.ytp-livebadge-color-control.ytp-persistent-scrubber.ytp-title-enable-channel-logo.ytp-embed.ytp-embed-playlist.unstarted-mode.ytp-pause-overlay-controls-hidden.ytp-small-mode
+div.html5-video-container
+div#player .ytp-show-cards-title .ytp-title-channel
+div#player .ytp-title-channel .ytp-title-expanded-overlay .ytp-title-expanded-heading
+button.btn.btn-outline-white.header_signin
+.form-group input#signupName
+.form-group input#signupLastName
+.hero-descriptor .hero-descriptor_descr.lead
+.form-group input#signupEmail, .form-group input#signupLastName, .form-group input#signupEmail, .form-group input#signupPassword
+.modal-footer .btn
+.modal-content .close
+.modal-body
+.form-group .form-control.ng-untouched.ng-pristine.ng-invalid
+p.hero-descriptor_descr.lead
+.header_logo
+div#contactsSection .contacts_socials.socials
+.section.about .about-block_picture.about-picture
+.section.contacts .socials_link
+.col-12.col-lg-6.mt-lg-0.mt-md-5.mt-sm-4.mt-3 .about-block .about-picture_img
+.socials_icon.icon.icon-telegram
+.contacts_socials.socials .socials_link
+.contacts_link.display-4
+.col.footer_item.-right
+p.about-block_title.h2
+
+
+#XPath
+//div[@class='hero-video']
+//div[@class='modal-content']
+//div[@class='modal-body']//*[@name='name']
+//div[@class='modal-body']//*[@name='name']/../..
+//div[@class='modal-content']/..//div[@class='form-group']/*[@name='repeatPassword']
+//div[@class='section about']//div[@class='about-block']//p[text()="Keep track of your replacement schedule and plan your vehicle maintenance expenses in advance."]
+//div[@class='section about']//div[@class='about-block']//p[contains(text(), "Keep track of your replacement schedule and plan your vehicle maintenance expenses in advance.")]
+//div[@class='section contacts']//div[@class='contacts_socials socials']
+//div[@class='contacts_socials socials']//span[@class = 'socials_icon icon icon-facebook']
+//div[@class='contacts_socials socials']//span[@class='socials_icon icon icon-telegram']
+//a[@class='contacts_link h4']
+//a[@class='contacts_link h4']/..//a[@class='contacts_link display-4']
+//p[text()='Hillel auto developed in Hillel IT school for educational purposes of QA courses.']/../../..//div[@class='col footer_item -right']
+//a[@class='contacts_link h4']/..//a[@class='contacts_link display-4']
+//p[@class='hero-descriptor_descr lead']/..//button[@class='hero-descriptor_btn btn btn-primary']
+//button[@class='btn btn-outline-white header_signin']/..//button[contains(text(), 'Guest log in')]
+//div[@class='app-content']//div[@class='guest-layout']//div[@class='row']
+//div[@class='col-12 col-lg-8']/../..//div[@class='col-12 col-lg-4']
+//div[@class='hero-descriptor']//h1[text()='Do more!']
+//h1[text()='Do more!']
+//script[@src='5.1b5e1a162b57c96ea901.js']/..//meta[@name='robots']
+//style[contains(text(), '.footer')]
+//style[contains(text(), '.footer')]/..//style[contains(text(), '.header_bar')]
+//script[@name='player/base']
+//a[@role='link']
